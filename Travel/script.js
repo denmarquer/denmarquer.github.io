3e6f562d898cd8666a3e4b95370d1adb24a39c25
denmarquer <40261343+denmarquer@users.noreply.github.com>
2018-06-22 19:57:12 +0600

    Add files via upload

---
+++ Travel/script.js
@@ -0,0 +1,7 @@
+jQuery(document).ready(function($){
+	$(".tab-item").not(":nth-child(2)").hide();
+	$(".order__offer .tab").click(function() {
+		$(".order__offer .tab").removeClass("active").eq($(this).index()).addClass("active");
+		$(".tab-item").hide().eq($(this).index()).fadeIn()
+	}).eq(1).addClass("active");
+});
